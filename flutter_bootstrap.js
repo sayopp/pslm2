29d096bd3ee3c9cacb40a986b0f1750c278470ed
sayopp <sayoxx@gmail.com>
2026-01-19 00:51:07 +0900

    14

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"78fc3012e45889657f72359b005af7beac47ba
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2942754223"
+    serviceWorkerVersion: "3966878690"
   }
 });
